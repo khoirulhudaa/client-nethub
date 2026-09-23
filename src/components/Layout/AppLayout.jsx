7030b00550f0b32571d6cafbdb61c5c187f347f4
dukungin <dukung.in.support@gmail.com>
2026-09-23 07:42:03 +0700

    fix

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -24,7 +24,8 @@ const AppLayout = () => {
       )}
 
 
-      <img src="/sidebar.png" alt="wallpaper-sidebar" className="absolute top-0 left-0 w-screen h-screen rotate-[4deg] scale-[2] w-full h-screen dark:flex hidden object-cover absolute z-0 top-0 opacity-5 left-0" />
+      <img src="/hero.jpg" alt="wallpaper-sidebar" className="absolute top-0 left-0 w-screen h-screen rotate-[4deg] scale-[2] w-full h-screen dark:flex hidden object-cover absolute z-0 top-0 opacity-15 left-0" />
+      {/* <img src="/hero.jpg" alt="wallpaper-sidebar" className="absolute top-0 left-0 w-screen h-screen rotate-[4deg] scale-[2] w-full h-screen dark:flex hidden object-cover absolute z-0 top-0 opacity-15 left-0" /> */}
       <div className="relative z-[9] flex min-w-0 flex-1 flex-col">
         <TopBar onMenuClick={() => setMobileOpen(!mobileOpen)} />
         <main className="flex-1 overflow-y-auto z-[99] px-4 py-4 md:py-0 md:pl-4 lg:pl-0 md:pr-[1px] rounded-none">
